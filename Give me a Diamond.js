168672643ea0f91503ea0785c0120ccf910bb4c9
I1yaEsenko <ilya.esenko@gmail.com>
2021-11-18 23:53:39 +0300

    Update Give me a Diamond.js

--- Give me a Diamond.js
+++ Give me a Diamond.js
@@ -1,14 +1,16 @@
 function diamond(n) {
 
 
-  if (n < 1 || n % 2 === 0) return null
+  if (n < 1 || n % 2 === 0) {
+    return null;
+  }
   let result = '',
     mid = Math.ceil(n / 2);
   for (let i = 1; i <= n; i++) {
     if (i <= mid) {
-      result += ' '.repeat(mid - i) + '*'.repeat(2 * i - 1) + '\n'
+      result += ' '.repeat(mid - i) + '*'.repeat(2 * i - 1) + '\n';
     } else {
-      result += ' '.repeat(i - mid) + '*'.repeat(2 * (n - i) + 1) + '\n'
+      result += ' '.repeat(i - mid) + '*'.repeat(2 * (n - i) + 1) + '\n';
     }
   }
   return result;
